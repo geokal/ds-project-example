943ca419f3e0823e3c77f985538e04710f3a3c81
George Kalpaktsoglou <gkalpak@fogus.gr>
2022-12-06 21:52:12 +0100

    Made a change

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,7 +4,7 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
-
+# Add a change
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
